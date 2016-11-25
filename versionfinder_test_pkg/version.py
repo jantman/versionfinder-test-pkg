212d9950ca546013b5539133d7862c2fa4bea6f3
Jason Antman <jason@jasonantman.com>
2016-11-25 09:41:25 -0500

    bump version from 0.2.4 to 0.2.5

--- versionfinder_test_pkg/version.py
+++ versionfinder_test_pkg/version.py
@@ -35,5 +35,5 @@ Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 ##################################################################################
 """
 
-VERSION = '0.2.4'
+VERSION = '0.2.5'
 PROJECT_URL = 'https://github.com/jantman/versionfinder-test-pkg'
